1832161a06fc0cb1aaf1ebe2af175609d10438c8
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2023-09-05 21:34:31 +0530

    Detail component created

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,5 +1,3 @@
-
-
 export const exerciseOptions = {
   method: "GET",
   headers: {
@@ -8,11 +6,17 @@ export const exerciseOptions = {
   },
 };
 
+ export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "79f0558b5emshaf559b0ca6daafbp114572jsnfc3edf53dc18",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
 
 export const fetchData = async (url, options) => {
-    const response = await fetch(url, options)
-    const data = await response.json();
+  const response = await fetch(url, options);
+  const data = await response.json();
 
-
-    return data;
-}
+  return data;
+};
